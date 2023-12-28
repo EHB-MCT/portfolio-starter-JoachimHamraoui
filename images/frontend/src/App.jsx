e20a110a58cd48b1085f15e44070afa49674fe8c
JoachimHamraoui <joachim.hamraoui@student.ehb.be>
2023-12-28 07:40:27 +0100

    feature(front-end) basic setup for client side of application

---
+++ images/frontend/src/App.jsx
@@ -0,0 +1,19 @@
+import './App.css';
+
+function App() {
+
+  return (
+    <>
+      <div className="w-9/12 m-auto px-2 py-4">
+        <div className='w-full p-2'>
+          <h1 className='text-3xl text-orange font-display font-bold'>Le Collectif</h1>
+        </div>
+        <div className='w-full flex flex-wrap flex-row'>
+          
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App
